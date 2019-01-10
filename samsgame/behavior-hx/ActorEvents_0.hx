97caea20afa12f96ada99dcaf1a3563e7ab49257
sfrantz499 <44300359+sfrantz499@users.noreply.github.com>
2019-01-10 10:54:26 -0800

    changed the game to boss level

---
+++ samsgame/behavior-hx/ActorEvents_0.hx
@@ -0,0 +1,171 @@
+package scripts;
+
+import com.stencyl.graphics.G;
+import com.stencyl.graphics.BitmapWrapper;
+
+import com.stencyl.behavior.Script;
+import com.stencyl.behavior.Script.*;
+import com.stencyl.behavior.ActorScript;
+import com.stencyl.behavior.SceneScript;
+import com.stencyl.behavior.TimedTask;
+
+import com.stencyl.models.Actor;
+import com.stencyl.models.GameModel;
+import com.stencyl.models.actor.Animation;
+import com.stencyl.models.actor.ActorType;
+import com.stencyl.models.actor.Collision;
+import com.stencyl.models.actor.Group;
+import com.stencyl.models.Scene;
+import com.stencyl.models.Sound;
+import com.stencyl.models.Region;
+import com.stencyl.models.Font;
+import com.stencyl.models.Joystick;
+
+import com.stencyl.Engine;
+import com.stencyl.Input;
+import com.stencyl.Key;
+import com.stencyl.utils.Utils;
+
+import openfl.ui.Mouse;
+import openfl.display.Graphics;
+import openfl.display.BlendMode;
+import openfl.display.BitmapData;
+import openfl.display.Bitmap;
+import openfl.events.Event;
+import openfl.events.KeyboardEvent;
+import openfl.events.TouchEvent;
+import openfl.net.URLLoader;
+
+import box2D.common.math.B2Vec2;
+import box2D.dynamics.B2Body;
+import box2D.dynamics.B2Fixture;
+import box2D.dynamics.joints.B2Joint;
+
+import motion.Actuate;
+import motion.easing.Back;
+import motion.easing.Cubic;
+import motion.easing.Elastic;
+import motion.easing.Expo;
+import motion.easing.Linear;
+import motion.easing.Quad;
+import motion.easing.Quart;
+import motion.easing.Quint;
+import motion.easing.Sine;
+
+import com.stencyl.graphics.shaders.BasicShader;
+import com.stencyl.graphics.shaders.GrayscaleShader;
+import com.stencyl.graphics.shaders.SepiaShader;
+import com.stencyl.graphics.shaders.InvertShader;
+import com.stencyl.graphics.shaders.GrainShader;
+import com.stencyl.graphics.shaders.ExternalShader;
+import com.stencyl.graphics.shaders.InlineShader;
+import com.stencyl.graphics.shaders.BlurShader;
+import com.stencyl.graphics.shaders.SharpenShader;
+import com.stencyl.graphics.shaders.ScanlineShader;
+import com.stencyl.graphics.shaders.CSBShader;
+import com.stencyl.graphics.shaders.HueShader;
+import com.stencyl.graphics.shaders.TintShader;
+import com.stencyl.graphics.shaders.BloomShader;
+
+
+
+class ActorEvents_0 extends ActorScript
+{
+	public var _touchfloor:Bool;
+	
+	
+	public function new(dummy:Int, actor:Actor, dummy2:Engine)
+	{
+		super(actor);
+		nameMap.set("touchfloor", "_touchfloor");
+		_touchfloor = false;
+		
+	}
+	
+	override public function init()
+	{
+		
+		/* ======================== When Updating ========================= */
+		addWhenUpdatedListener(null, function(elapsedTime:Float, list:Array<Dynamic>):Void
+		{
+			if(wrapper.enabled)
+			{
+				engine.cameraFollow(actor);
+				if(isKeyDown("right"))
+				{
+					actor.setXVelocity(15);
+					actor.setAnimation("" + "WalkRight");
+				}
+				else if(isKeyDown("left"))
+				{
+					actor.setXVelocity(-15);
+					actor.setAnimation("" + "WalkLeft");
+				}
+				else if(isKeyReleased("left"))
+				{
+					actor.setXVelocity(0);
+				}
+				else if(isKeyReleased("right"))
+				{
+					actor.setXVelocity(0);
+				}
+			}
+		});
+		
+		/* ======================= Member of Group ======================== */
+		addCollisionListener(actor, function(event:Collision, list:Array<Dynamic>):Void
+		{
+			if(wrapper.enabled && sameAsAny(getActorGroup(1),event.otherActor.getType(),event.otherActor.getGroup()))
+			{
+				_touchfloor = true;
+				propertyChanged("_touchfloor", _touchfloor);
+			}
+		});
+		
+		/* ======================== When Updating ========================= */
+		addWhenUpdatedListener(null, function(elapsedTime:Float, list:Array<Dynamic>):Void
+		{
+			if(wrapper.enabled)
+			{
+				if((isKeyDown("up") && (_touchfloor == true)))
+				{
+					actor.setYVelocity(-20);
+					_touchfloor = false;
+					propertyChanged("_touchfloor", _touchfloor);
+				}
+			}
+		});
+		
+		/* =========================== Keyboard =========================== */
+		addKeyStateListener("action1", function(pressed:Bool, released:Bool, list:Array<Dynamic>):Void
+		{
+			if(wrapper.enabled && pressed)
+			{
+				createRecycledActor(getActorType(11), actor.getX(), actor.getY(), Script.FRONT);
+				if((actor.getAnimation() == "WalkLeft"))
+				{
+					getLastCreatedActor().applyImpulse(-1, 0, 85);
+				}
+				else if((actor.getAnimation() == "WalkRight"))
+				{
+					getLastCreatedActor().applyImpulse(1, 0, 85);
+				}
+			}
+		});
+		
+		/* ======================== Actor of Type ========================= */
+		addCollisionListener(actor, function(event:Collision, list:Array<Dynamic>):Void
+		{
+			if(wrapper.enabled && sameAsAny(getActorType(6), event.otherActor.getType(),event.otherActor.getGroup()))
+			{
+				recycleActor(actor);
+			}
+		});
+		
+	}
+	
+	override public function forwardMessage(msg:String)
+	{
+		
+	}
+}
